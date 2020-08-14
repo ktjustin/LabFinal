be2069597e1c1a3b85f5e4c5b90ca92738f91e06
Ubuntu <ktjustin@gmail.com>
2020-08-14 16:49:36 +0000

    updated version

--- src/myApp/HelloWorld.java
+++ src/myApp/HelloWorld.java
@@ -2,7 +2,7 @@ package myApp;
 
 public class HelloWorld {
 	public static void main(String[] args) {
-		System.out.println("Hello world from my FinalApp!");
+		System.out.println("Hello world from my FinalApp version 2!");
     }
 
 }
